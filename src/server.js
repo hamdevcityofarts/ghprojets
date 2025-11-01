1f4bb1106823c4e578381bde04ce146315966e6e
hamdevcityofarts <hamnsanf@gmail.com>
2025-11-01 15:29:26 +0100

    remove hard coding cors origine replace by ALLOWEDORIGIN env

--- src/server.js
+++ src/server.js
@@ -14,15 +14,10 @@ dotenv.config();
 const app = express();
 
 // ✅ CONFIGURATION CORS AMÉLIORÉE
+const allowedOrigin = process.env.ALLOWEDORIGIN?.split(',') || []
+
 const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:5173',
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:3001',
-    'http://127.0.0.1:5173'   
-  ],
+  origin: allowedOrigin,
   credentials: true,
   optionsSuccessStatus: 200
 };
